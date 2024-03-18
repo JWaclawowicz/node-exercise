de1b446af51a0a0428110ea962dd443e31069f03
Justyna <justyna.waclawowicz@gmail.com>
2024-03-18 20:34:42 +0100

    extract EventParser to separate file and add some basic unit tests

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,6 @@
 import { MatchData } from './interfaces/match-data.interface';
 import { ParsedMatchData } from './interfaces/parsed-match-data.interface';
-import { isScoreString, extractScoreListFromString, isScoreList } from './utils/score.utils';
-
-class EventParser {
-  makeEventName(match: MatchData): string {
-    if (match.sport === 'soccer') {
-      return match.participant1 + ' - ' + match.participant2;
-    } else if (match.sport === 'tennis') {
-      return match.participant1 + ' vs ' + match.participant2;
-    } else if (match.sport === 'volleyball') {
-      return match.participant1 + ' - ' + match.participant2;
-    } else if (match.sport === 'handball') {
-      return match.participant1 + ' vs ' + match.participant2;
-    } else if (match.sport === 'basketball') {
-      return match.participant1 + ' - ' + match.participant2;
-    } else {
-      return 'Exception: invalid sport';
-    }
-  }
-
-  formatScore(match: MatchData): string {
-    if (match.sport === 'soccer') {
-      if (!isScoreString(match.score)) return '';
-      return match.score;
-    } else if (match.sport === 'tennis') {
-      if (!isScoreString(match.score)) return '';
-
-      const scores = extractScoreListFromString(match.score);
-      if (!scores) return '';
-
-      const set1 = scores[2];
-      const set2 = scores[3];
-      const set3 = scores[4];
-
-      return 'Main score: ' + scores[1] + ' (' + 'set1 ' + set1 + ', ' + 'set2 ' + set2 + ', ' + 'set3 ' + set3 + ')';
-    } else if (match.sport === 'volleyball') {
-      if (!isScoreString(match.score)) return '';
-
-      const scores = extractScoreListFromString(match.score);
-      if (!scores) return '';
-
-      const set1 = scores[2];
-      const set2 = scores[3];
-      const set3 = scores[4];
-
-      return 'Main score: ' + scores[1] + ' (' + 'set1 ' + set1 + ', ' + 'set2 ' + set2 + ', ' + 'set3 ' + set3 + ')';
-    } else if (match.sport === 'basketball') {
-      if (!isScoreList(match.score)) return '';
-      return match.score[0][0] + ',' + match.score[0][1] + ',' + match.score[1][0] + ',' + match.score[1][1];
-    } else if (match.sport === 'handball') {
-      if (!isScoreString(match.score)) return '';
-      return match.score;
-    } else {
-      return 'Exception: invalid sport';
-    }
-  }
-}
+import { EventParser } from './parsers/event-parser';
 
 export function getParsedMatches(matches: MatchData[]): ParsedMatchData[] {
   const matchesParsed = [];
---
+++ src/tests/parsers/event-parser.test.ts
@@ -0,0 +1,176 @@
+import { MatchData } from '../../interfaces/match-data.interface';
+import { EventParser } from '../../parsers/event-parser';
+
+describe('EventParser', (): void => {
+  let parser: EventParser;
+
+  beforeEach((): void => {
+    parser = new EventParser();
+  });
+
+  describe('makeEventName', (): void => {
+    describe('when sport is soccer', (): void => {
+      it('should return the event name with a dash', (): void => {
+        const match: MatchData = {
+          sport: 'soccer',
+          participant1: 'Chelsea',
+          participant2: 'Arsenal',
+          score: '2:1',
+        };
+        expect(parser.makeEventName(match)).toBe('Chelsea - Arsenal');
+      });
+    });
+
+    describe('when sport is volleyball', (): void => {
+      it('should return the event name with a dash', (): void => {
+        const match: MatchData = {
+          sport: 'volleyball',
+          participant1: 'Germany',
+          participant2: 'France',
+          score: '3:0,25:23,25:19,25:21',
+        };
+        expect(parser.makeEventName(match)).toBe('Germany - France');
+      });
+    });
+
+    describe('when sport is handball', (): void => {
+      it('should return the event name with the string vs', (): void => {
+        const match: MatchData = {
+          sport: 'handball',
+          participant1: 'Pogoń Szczeciń',
+          participant2: 'Azoty Puławy',
+          score: '34:26',
+        };
+        expect(parser.makeEventName(match)).toBe('Pogoń Szczeciń vs Azoty Puławy');
+      });
+    });
+
+    describe('when sport is basketball', (): void => {
+      it('should return the event name with a dash', (): void => {
+        const match: MatchData = {
+          sport: 'basketball',
+          participant1: 'GKS Tychy',
+          participant2: 'GKS Katowice',
+          score: [
+            ['9:7', '2:1'],
+            ['5:3', '9:9'],
+          ],
+        };
+        expect(parser.makeEventName(match)).toBe('GKS Tychy - GKS Katowice');
+      });
+    });
+
+    describe('when sport is tennis', (): void => {
+      it('should return the event name with the string vs', (): void => {
+        const match: MatchData = {
+          sport: 'tennis',
+          participant1: 'Maria Sharapova',
+          participant2: 'Serena Williams',
+          score: '2:1,7:6,6:3,6:7',
+        };
+        expect(parser.makeEventName(match)).toBe('Maria Sharapova vs Serena Williams');
+      });
+    });
+
+    describe('when sport is ski jumping', (): void => {
+      it('should return an exception message', (): void => {
+        const match: MatchData = {
+          sport: 'ski jumping',
+        };
+        expect(parser.makeEventName(match)).toBe('Exception: invalid sport');
+      });
+    });
+
+    describe('when sport is not available', (): void => {
+      it('should return an exception message', (): void => {
+        const match: MatchData = {
+          sport: '',
+        };
+        expect(parser.makeEventName(match)).toBe('Exception: invalid sport');
+      });
+    });
+  });
+
+  describe('formatScore', (): void => {
+    describe('when sport is soccer', (): void => {
+      it('should return a single score', (): void => {
+        const match: MatchData = {
+          sport: 'soccer',
+          participant1: 'Chelsea',
+          participant2: 'Arsenal',
+          score: '2:1',
+        };
+        expect(parser.formatScore(match)).toBe('2:1');
+      });
+    });
+
+    describe('when sport is volleyball', (): void => {
+      it('should return a main score and scores of individual sets', (): void => {
+        const match: MatchData = {
+          sport: 'volleyball',
+          participant1: 'Germany',
+          participant2: 'France',
+          score: '3:0,25:23,25:19,25:21',
+        };
+        expect(parser.formatScore(match)).toBe('Main score: 3:0 (set1 25:23, set2 25:19, set3 25:21)');
+      });
+    });
+
+    describe('when sport is handball', (): void => {
+      it('should return a single score', (): void => {
+        const match: MatchData = {
+          sport: 'handball',
+          participant1: 'Pogoń Szczeciń',
+          participant2: 'Azoty Puławy',
+          score: '34:26',
+        };
+        expect(parser.formatScore(match)).toBe('34:26');
+      });
+    });
+
+    describe('when sport is basketball', (): void => {
+      it('should return multiple scores', (): void => {
+        const match: MatchData = {
+          sport: 'basketball',
+          participant1: 'GKS Tychy',
+          participant2: 'GKS Katowice',
+          score: [
+            ['9:7', '2:1'],
+            ['5:3', '9:9'],
+          ],
+        };
+        expect(parser.formatScore(match)).toBe('9:7,2:1,5:3,9:9');
+      });
+    });
+
+    describe('when sport is tennis', (): void => {
+      it('should return a main score and scores of individual sets', (): void => {
+        const match: MatchData = {
+          sport: 'tennis',
+          participant1: 'Maria Sharapova',
+          participant2: 'Serena Williams',
+          score: '2:1,7:6,6:3,6:7',
+        };
+        expect(parser.formatScore(match)).toBe('Main score: 2:1 (set1 7:6, set2 6:3, set3 6:7)');
+      });
+    });
+
+    describe('when sport is ski jumping', (): void => {
+      it('should return an exception message', (): void => {
+        const match: MatchData = {
+          sport: 'ski jumping',
+        };
+        expect(parser.formatScore(match)).toBe('Exception: invalid sport');
+      });
+    });
+
+    describe('when sport is not available', (): void => {
+      it('should return an exception message', (): void => {
+        const match: MatchData = {
+          sport: '',
+        };
+        expect(parser.formatScore(match)).toBe('Exception: invalid sport');
+      });
+    });
+  });
+});
